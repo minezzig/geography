2c447199ea15d38d8b5df40e89359df6d993c503
Greg <gregoryminezzi@gmail.com>
2023-07-14 19:42:27 -0400

    add repsponsive navigation

--- app/header.js
+++ app/header.js
@@ -1,3 +1,39 @@
+"use client";
+
+import styles from "./header.module.css";
+
 export default function Header() {
-    return <h3>header</h3>
+  function handleClick() {
+    const navLinks = document.querySelector(`.${styles.navLinks}`);
+    const bars = document.querySelectorAll(`.${styles.bar}`);
+    navLinks.classList.toggle(`${styles.open}`);
+    bars.forEach((bar) => bar.classList.toggle(`${styles.x}`));
+  }
+  return (
+    <>
+      <nav className={styles.navBar}>
+        <div className="logo">
+          <h1>
+            <a href="/">Geography</a>
+          </h1>
+        </div>
+        <div className={styles.hamburger} onClick={handleClick}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+        <ul className={styles.navLinks}>
+          <li>
+            <a href="/game">Game</a>
+          </li>
+          <li>
+            <a href="/search">Search</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
